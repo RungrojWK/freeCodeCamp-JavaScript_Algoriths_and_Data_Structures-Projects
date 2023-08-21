c5201549598be1076c4ceb626b36d1dfe065843b
Rungroj Wongkaeo <63869763+RungrojWK@users.noreply.github.com>
2023-08-21 16:18:16 +0700

    Create Solution.js

---
+++ Palindrome_Checker/Solution.js
@@ -0,0 +1,29 @@
+function palindrome(str) {
+  const strReplace = str.replace(/[^a-zA-Z0-9]/g, '')
+  return strReplace.split('').reverse().join("").toLowerCase() == strReplace.toLowerCase();
+}
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const main = () => {
+  console.log('Key _out for close processing')
+
+  rl.question("Enter a string: ", (str) => {
+
+    if(str.trim().toLocaleLowerCase() != '_out') {
+      console.log('Result is ', palindrome(str), '\n');
+      main();   
+    } else {
+      rl.close()
+    }
+
+  })
+}
+
+main();
